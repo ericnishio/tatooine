7c450bb3dcb4efb719fbf5454e74f3d530da64f8
Eric Nishio <eric@self-learner.com>
2015-02-21 18:13:50 +0200

    Add blank favicon

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta name="viewport" content="width=device-width" />
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width">
   <title><?php wp_title( ' | ', true, 'right' ); ?></title>
-  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
