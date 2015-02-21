4e0ac6ed7dccadf6b591dbbf6490df3474efda2c
Eric Nishio <eric@self-learner.com>
2015-02-22 00:37:35 +0200

    Style older/newer navigation links

--- nav-below.php
+++ nav-below.php
@@ -1,6 +1,9 @@
-<?php global $wp_query; if ( $wp_query->max_num_pages > 1 ) { ?>
-<nav id="nav-below" class="navigation" role="navigation">
-<div class="nav-previous"><?php next_posts_link(sprintf( __( '%s older', 'tatooine' ), '<span class="meta-nav">&larr;</span>' ) ) ?></div>
-<div class="nav-next"><?php previous_posts_link(sprintf( __( 'newer %s', 'tatooine' ), '<span class="meta-nav">&rarr;</span>' ) ) ?></div>
-</nav>
-<?php } ?>
+  <?php global $wp_query; ?>
+  <?php if ( $wp_query->max_num_pages > 1 ): ?>
+    <section class="section section-secondary section-container section-more-padding">
+      <nav id="nav-below" class="page-navigation" role="navigation">
+        <div class="nav-previous"><?php next_posts_link(sprintf( __( '%s older', 'tatooine' ), '<i class="fa fa-angle-left"></i>' ) ); ?></div>
+        <div class="nav-next"><?php previous_posts_link(sprintf( __( 'newer %s', 'tatooine' ), '<i class="fa fa-angle-right"></i>' ) ); ?></div>
+      </nav>
+    </section>
+  <?php endif; ?>
