d0390f60903164d56b912c043aa84e4dd33ce86a
Eric Nishio <eric@self-learner.com>
2015-02-22 00:55:08 +0200

    Style pages

--- page.php
+++ page.php
@@ -1,17 +1,37 @@
 <?php get_header(); ?>
-<section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
-</article>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+  <section class="section section-narrow">
+    <div class="section-inner">
+      <section id="content" class="entry" role="main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <div class="meta-info">
+            <?php if ( is_post_author() ): ?>
+              <div class="meta-item">
+                <div class="vertical-align">
+                  <i class="fa fa-pencil vertical-align-inner"></i>
+                  <span class="meta-text vertical-align-inner">
+                    <?php edit_post_link(); ?>
+                  </span>
+                </div>
+              </div>
+            <?php endif; ?>
+          </div>
+          <section class="entry-content">
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+            <?php the_content(); ?>
+            <div class="entry-links">
+              <?php wp_link_pages(); ?>
+            </div>
+          </section>
+        </article>
+      </section>
+    </div>
+  </section>
+  <section class="section-secondary section-container">
+    <div class="section-inner">
+      <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+    </div>
+  </section>
 <?php endwhile; endif; ?>
-</section>
 <?php get_footer(); ?>
