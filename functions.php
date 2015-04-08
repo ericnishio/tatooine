41b4b520bef5b2f8be7d968a2c2c0a70f46e6064
Eric Nishio <eric@self-learner.com>
2015-04-08 11:01:59 +0300

    Fix conditional script loading

--- functions.php
+++ functions.php
@@ -15,7 +15,15 @@ add_action( 'wp_enqueue_scripts', 'tatooine_load_scripts' );
 function tatooine_load_scripts()
 {
   wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'html5shiv', 'https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js' );
 }
+
+add_filter( 'script_loader_tag', function( $tag, $handle) {
+  if ( $handle === 'html5shiv' ) {
+    $tag = "<!--[if lt IE 9]>$tag<![endif]-->";
+  }
+  return $tag;
+}, 10, 2 );
 
 add_action( 'comment_form_before', 'tatooine_enqueue_comment_reply_script' );
 function tatooine_enqueue_comment_reply_script()
--- header.php
+++ header.php
@@ -8,9 +8,6 @@
   <link href="http://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet" type="text/css">
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
-  <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js" type="text/javascript"></script>
-  <![endif]-->
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
