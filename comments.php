c161ad17b1e8f3d5cb5cf5438e8a60a4ed57ed76
Eric Nishio <ericnishio@protonmail.com>
2017-02-12 01:56:07 +0200

    Update main menu styles

--- comments.php
+++ comments.php
@@ -1,35 +1,40 @@
 <?php if ( 'comments.php' == basename( $_SERVER['SCRIPT_FILENAME'] ) ) return; ?>
 <section id="comments" class="comments">
+  <?php if (! have_comments() ): ?>
+    <section class="section section-container">
+      <div class="section-inner"></div>
+    </section>
+  <?php endif; ?>
   <?php if ( have_comments() ): ?>
     <?php global $comments_by_type; ?>
     <?php $comments_by_type = &separate_comments( $comments ); ?>
     <?php if ( ! empty( $comments_by_type['comment'] ) ): ?>
       <section class="section section-container">
-          <div class="section-inner">
-            <section id="comments-list" class="comments">
-              <h3 class="comments-heading"><?php comments_number(); ?></h3>
-              <?php if ( get_comment_pages_count() > 1 ) : ?>
-                <nav id="comments-nav-above" class="comments-navigation" role="navigation">
-                  <div class="paginated-comments-links">
-                    <?php paginate_comments_links(); ?>
-                  </div>
-                </nav>
-              <?php endif; ?>
-              <ul>
-              <?php wp_list_comments( array(
-                'callback' => 'comments_callback',
-              ) ); ?>
-              </ul>
-              <?php if ( get_comment_pages_count() > 1 ) : ?>
-                <nav id="comments-nav-below" class="comments-navigation" role="navigation">
-                  <div class="paginated-comments-links">
-                    <?php paginate_comments_links(); ?>
-                  </div>
-                </nav>
-              <?php endif; ?>
-            </section>
-          </div>
-        </section>
+        <div class="section-inner">
+          <section id="comments-list" class="comments">
+            <h3 class="comments-heading"><?php comments_number(); ?></h3>
+            <?php if ( get_comment_pages_count() > 1 ) : ?>
+              <nav id="comments-nav-above" class="comments-navigation" role="navigation">
+                <div class="paginated-comments-links">
+                  <?php paginate_comments_links(); ?>
+                </div>
+              </nav>
+            <?php endif; ?>
+            <ul>
+            <?php wp_list_comments( array(
+              'callback' => 'comments_callback',
+            ) ); ?>
+            </ul>
+            <?php if ( get_comment_pages_count() > 1 ) : ?>
+              <nav id="comments-nav-below" class="comments-navigation" role="navigation">
+                <div class="paginated-comments-links">
+                  <?php paginate_comments_links(); ?>
+                </div>
+              </nav>
+            <?php endif; ?>
+          </section>
+        </div>
+      </section>
     <?php endif; ?>
     <?php if ( ! empty( $comments_by_type['pings'] ) ): ?>
       <?php $ping_count = count( $comments_by_type['pings'] ); ?>
